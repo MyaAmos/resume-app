a4840a225a8261f616b1ba451b969c126a6628eb
MyaAmos <peacockmya@gmail.com>
2022-05-23 15:05:56 -0400

    changed a tag in header to navlinks

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function HeaderComponent() {
   return (
     <div>
         <nav className='navbar navbar-expand navbar-light bg-light'>
-            <a className='navbar-brand' href='#'>Mya Amos</a>
+            <a className='navbar-brand' href='https://www.linkedin.com/in/mya-amos-1305291bb/'>Mya Amos</a>
 
             <div>
                 <ul className='navbar-nav mr-auto'>
                     <li className='nav-item'>
-                        <a className='nav-link' href='/'>Home</a>
+                        <NavLink className='nav-link' to='/'>Home</NavLink>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link' href='/experience'>Experience</a>
+                        <NavLink className='nav-link' to='/experience'>Experience</NavLink>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link' href='/education'>Education</a>
+                        <NavLink className='nav-link' to='/education'>Education</NavLink>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link' href='/aboutme'>About Me</a>
+                        <NavLink className='nav-link' to='/aboutme'>About Me</NavLink>
                     </li>
                 </ul>
             </div>
